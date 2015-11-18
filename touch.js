04ee96ee9ba3065e18b718f825ff9ef86e231d90
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-11-18 20:11:39 +1300

    Styles
    Moved to stylesheet and reset player positions

--- touch.js
+++ touch.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	console.log('a')
+	$('body').append('<div class="container"></div>')
+	scoringPlay()
 })
+
+function scoringPlay() {
+	$('.container').children().remove()
+	$('.container').append('<div id="scoringPlay"></div>')
+	createPlayers('defence', 3)
+	createPlayers('offence', 3)
+}
+
+function createPlayers(type, players) {
+	for (var i = 1; i <= players; i++) {
+		$('.container').children().append('<div id="'+type+i+'" class="'+type+' player"></div>')
+	};
+}
