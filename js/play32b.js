2dcaf6d5f7798d5d816fb3ea483e8967acc5617f
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-11-23 13:53:37 +1300

    adjusted based on feedback

--- js/play32b.js
+++ js/play32b.js
@@ -1,4 +1,95 @@
-play32bPlay = [{'id':'defence1','positions':[{'top':'50px','left':'30px','speed':0,'color':'#FF9900'},{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'0px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'0px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'0px','speed':1200,'color':'#FF9900'}]},{'id':'defence2','positions':[{'top':'50px','left':'110px','speed':0,'color':'#FF9900'},{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'60px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'}]},{'id':'defence3','positions':[{'top':'50px','left':'190px','speed':0,'color':'#FF9900'},{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'230px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'}]},{'id':'defence4','positions':[{'top':'50px','left':'270px','speed':0,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'}]},{'id':'defence5','positions':[{'top':'50px','left':'350px','speed':0,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'}]},{'id':'defence6','positions':[{'top':'50px','left':'430px','speed':0,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'}]},{'id':'offence1','positions':[{'top':'300px','left':'30px','speed':0,'color':'#389944'},{'top':'300px','left':'30px','speed':1200,'color':'#389944'},{'top':'300px','left':'30px','speed':1200,'color':'#389944'},{'top':'240px','left':'0px','speed':1200,'color':'#389944'},{'top':'200px','left':'0px','speed':1200,'color':'#389944'},{'top':'200px','left':'0px','speed':1200,'color':'#389944'}]},{'id':'offence2','positions':[{'top':'300px','left':'110px','speed':0,'color':'#389944'},{'top':'300px','left':'110px','speed':1200,'color':'#389944'},{'top':'300px','left':'110px','speed':1200,'color':'#389944'},{'top':'240px','left':'60px','speed':1200,'color':'#389944'},{'top':'200px','left':'80px','speed':1200,'color':'#384D99'},{'top':'0px','left':'150px','speed':1200,'color':'#384D99'}]},{'id':'offence3','positions':[{'top':'300px','left':'190px','speed':0,'color':'#389944'},{'top':'300px','left':'190px','speed':1200,'color':'#389944'},{'top':'300px','left':'190px','speed':1200,'color':'#389944'},{'top':'240px','left':'150px','speed':1200,'color':'#389944'},{'top':'160px','left':'210px','speed':1200,'color':'#389944'},{'top':'160px','left':'210px','speed':1200,'color':'#389944'}]},{'id':'offence4','positions':[{'top':'300px','left':'270px','speed':0,'color':'#384D99'},{'top':'200px','left':'310px','speed':1200,'color':'#384D99'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'}]},{'id':'offence5','positions':[{'top':'300px','left':'350px','speed':0,'color':'#389944'},{'top':'240px','left':'310px','speed':1200,'color':'#389944'},{'top':'240px','left':'310px','speed':1200,'color':'#384D99'},{'top':'200px','left':'270px','speed':1200,'color':'#384D99'},{'top':'160px','left':'270px','speed':1200,'color':'#389944'},{'top':'160px','left':'270px','speed':1200,'color':'#389944'}]},{'id':'offence6','positions':[{'top':'300px','left':'430px','speed':0,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'}]},{'id':'ball','positions':[{'top':'303px','left':'275px','speed':0,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'243px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'275px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'85px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'3px','left':'155px','speed':1200,'color':'rgba(255,255,255,0)'}]}]
+play32bPlay = [
+{'id':'defence1','positions':[
+	{'top':'50px','left':'30px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'0px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'0px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'0px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence2','positions':[
+	{'top':'50px','left':'110px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'170px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence3','positions':[
+	{'top':'50px','left':'190px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'230px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence4','positions':[
+	{'top':'50px','left':'270px','speed':0,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence5','positions':[
+	{'top':'50px','left':'350px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence6','positions':[
+	{'top':'50px','left':'430px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'}]},
+{'id':'offence1','positions':[
+	{'top':'300px','left':'30px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'30px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'30px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'0px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'0px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'0px','speed':1200,'color':'#389944'}]},
+{'id':'offence2','positions':[
+	{'top':'300px','left':'110px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'110px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'110px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'60px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'60px','speed':1200,'color':'#384D99'},
+	{'top':'0px','left':'140px','speed':1200,'color':'#384D99'}]},
+{'id':'offence3','positions':[
+	{'top':'300px','left':'190px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'190px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'190px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'130px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'210px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'210px','speed':1200,'color':'#389944'}]},
+{'id':'offence4','positions':[
+	{'top':'300px','left':'270px','speed':0,'color':'#384D99'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#384D99'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'}]},
+{'id':'offence5','positions':[
+	{'top':'300px','left':'350px','speed':0,'color':'#389944'},
+	{'top':'240px','left':'310px','speed':1200,'color':'#389944'},
+	{'top':'240px','left':'310px','speed':1200,'color':'#384D99'},
+	{'top':'200px','left':'270px','speed':1200,'color':'#384D99'},
+	{'top':'160px','left':'270px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'270px','speed':1200,'color':'#389944'}]},
+{'id':'offence6','positions':[
+	{'top':'300px','left':'430px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'}]},
+{'id':'ball','positions':[
+	{'top':'303px','left':'275px','speed':0,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'243px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'275px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'65px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'3px','left':'145px','speed':1200,'color':'rgba(255,255,255,0)'}]}]
 play32bPlayers = 6
 play32bPositions = ['W', 'L', 'M', 'M', 'L', 'W']
 
@@ -20,11 +111,11 @@ function setUpplay32b() {
 }
 
 function play32b() {
-	$('.play32bInit').on('click', function() {
+	$(document.body).on('click', '.play32bInit', function() {
 		$('#startButton').hide()
 		runplay32b()
 	})
-	$('.play32bReset').on('click', function() {
+	$(document.body).on('click', '.play32bReset', function() {
 		resetViewerWindow()
 		setUpplay32b()
 		setPositions(play32bPlay)
--- js/play32c.js
+++ js/play32c.js
@@ -1,4 +1,95 @@
-play32cPlay = [{'id':'defence1','positions':[{'top':'50px','left':'30px','speed':0,'color':'#FF9900'},{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'0px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'120px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'120px','speed':1200,'color':'#FF9900'}]},{'id':'defence2','positions':[{'top':'50px','left':'110px','speed':0,'color':'#FF9900'},{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'60px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'}]},{'id':'defence3','positions':[{'top':'50px','left':'190px','speed':0,'color':'#FF9900'},{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},{'top':'120px','left':'230px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'},{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'}]},{'id':'defence4','positions':[{'top':'50px','left':'270px','speed':0,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'}]},{'id':'defence5','positions':[{'top':'50px','left':'350px','speed':0,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'}]},{'id':'defence6','positions':[{'top':'50px','left':'430px','speed':0,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'}]},{'id':'offence1','positions':[{'top':'300px','left':'30px','speed':0,'color':'#389944'},{'top':'300px','left':'30px','speed':1200,'color':'#389944'},{'top':'300px','left':'30px','speed':1200,'color':'#389944'},{'top':'240px','left':'0px','speed':1200,'color':'#389944'},{'top':'200px','left':'0px','speed':1200,'color':'#384D99'},{'top':'0px','left':'0px','speed':1200,'color':'#384D99'}]},{'id':'offence2','positions':[{'top':'300px','left':'110px','speed':0,'color':'#389944'},{'top':'300px','left':'110px','speed':1200,'color':'#389944'},{'top':'300px','left':'110px','speed':1200,'color':'#389944'},{'top':'240px','left':'60px','speed':1200,'color':'#389944'},{'top':'160px','left':'150px','speed':1200,'color':'#389944'},{'top':'160px','left':'150px','speed':1200,'color':'#389944'}]},{'id':'offence3','positions':[{'top':'300px','left':'190px','speed':0,'color':'#389944'},{'top':'300px','left':'190px','speed':1200,'color':'#389944'},{'top':'300px','left':'190px','speed':1200,'color':'#389944'},{'top':'240px','left':'150px','speed':1200,'color':'#389944'},{'top':'160px','left':'210px','speed':1200,'color':'#389944'},{'top':'160px','left':'210px','speed':1200,'color':'#389944'}]},{'id':'offence4','positions':[{'top':'300px','left':'270px','speed':0,'color':'#384D99'},{'top':'200px','left':'310px','speed':1200,'color':'#384D99'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'},{'top':'200px','left':'310px','speed':1200,'color':'#202020'}]},{'id':'offence5','positions':[{'top':'300px','left':'350px','speed':0,'color':'#389944'},{'top':'240px','left':'310px','speed':1200,'color':'#389944'},{'top':'240px','left':'310px','speed':1200,'color':'#384D99'},{'top':'200px','left':'270px','speed':1200,'color':'#384D99'},{'top':'160px','left':'270px','speed':1200,'color':'#389944'},{'top':'160px','left':'270px','speed':1200,'color':'#389944'}]},{'id':'offence6','positions':[{'top':'300px','left':'430px','speed':0,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'},{'top':'300px','left':'430px','speed':1200,'color':'#389944'}]},{'id':'ball','positions':[{'top':'303px','left':'275px','speed':0,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'243px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'275px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'203px','left':'5px','speed':1200,'color':'rgba(255,255,255,0)'},{'top':'3px','left':'5px','speed':1200,'color':'rgba(255,255,255,0)'}]}]
+play32cPlay = [
+{'id':'defence1','positions':[
+	{'top':'50px','left':'30px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'30px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'110px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'120px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'120px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence2','positions':[
+	{'top':'50px','left':'110px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'110px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'170px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'180px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence3','positions':[
+	{'top':'50px','left':'190px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'190px','speed':1200,'color':'#FF9900'},
+	{'top':'120px','left':'230px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'},
+	{'top':'135px','left':'240px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence4','positions':[
+	{'top':'50px','left':'270px','speed':0,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'},
+	{'top':'160px','left':'310px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence5','positions':[
+	{'top':'50px','left':'350px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'350px','speed':1200,'color':'#FF9900'}]},
+{'id':'defence6','positions':[
+	{'top':'50px','left':'430px','speed':0,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'},
+	{'top':'50px','left':'430px','speed':1200,'color':'#FF9900'}]},
+{'id':'offence1','positions':[
+	{'top':'300px','left':'30px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'30px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'30px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'0px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'0px','speed':1200,'color':'#384D99'},
+	{'top':'0px','left':'0px','speed':1200,'color':'#384D99'}]},
+{'id':'offence2','positions':[
+	{'top':'300px','left':'110px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'110px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'110px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'60px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'150px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'150px','speed':1200,'color':'#389944'}]},
+{'id':'offence3','positions':[
+	{'top':'300px','left':'190px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'190px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'190px','speed':1200,'color':'#389944'},
+	{'top':'200px','left':'130px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'210px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'210px','speed':1200,'color':'#389944'}]},
+{'id':'offence4','positions':[
+	{'top':'300px','left':'270px','speed':0,'color':'#384D99'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#384D99'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'},
+	{'top':'200px','left':'310px','speed':1200,'color':'#202020'}]},
+{'id':'offence5','positions':[
+	{'top':'300px','left':'350px','speed':0,'color':'#389944'},
+	{'top':'240px','left':'310px','speed':1200,'color':'#389944'},
+	{'top':'240px','left':'310px','speed':1200,'color':'#384D99'},
+	{'top':'200px','left':'270px','speed':1200,'color':'#384D99'},
+	{'top':'160px','left':'270px','speed':1200,'color':'#389944'},
+	{'top':'160px','left':'270px','speed':1200,'color':'#389944'}]},
+{'id':'offence6','positions':[
+	{'top':'300px','left':'430px','speed':0,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'},
+	{'top':'300px','left':'430px','speed':1200,'color':'#389944'}]},
+{'id':'ball','positions':[
+	{'top':'303px','left':'275px','speed':0,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'243px','left':'315px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'275px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'203px','left':'5px','speed':1200,'color':'rgba(255,255,255,0)'},
+	{'top':'3px','left':'5px','speed':1200,'color':'rgba(255,255,255,0)'}]}]
 play32cPlayers = 6
 play32cPositions = ['W', 'L', 'M', 'M', 'L', 'W']
 
@@ -20,11 +111,11 @@ function setUpplay32c() {
 }
 
 function play32c() {
-	$('.play32cInit').on('click', function() {
+	$(document.body).on('click', '.play32cInit', function() {
 		$('#startButton').hide()
 		runplay32c()
 	})
-	$('.play32cReset').on('click', function() {
+	$(document.body).on('click', '.play32cReset', function() {
 		resetViewerWindow()
 		setUpplay32c()
 		setPositions(play32cPlay)
